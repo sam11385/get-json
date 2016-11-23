83b9e2ca5a21c906ae803fdac5a4d94039c62314
sam11385 <samluedke@gmail.com>
2016-11-23 16:54:20 -0500

    Getting more json

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
 // Let us get the json
+(function() {
 
-$.getJSON( "epl.json", function( data ) {
-  var items = [];
+	var apiData = "epl.json";
+	$.getJSON( apiData, {
+		format: "json"
+	})
+
+	.done(function( data ) {
+      $.each( data.items, function( key, val ) {
+        $( "<div>" ).attr( "src", item.media.m ).appendTo( "#list" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+
+
+})();
+
+	
+// 	function( data ) {
+//   	var items = [];
   
-  JSON.stringify(data);
+//   // JSON.stringify(data, function(key,val) {
+//   // 	items.push( "<li id='" + key[0] + "'>" + val[0] + "</li>" );
+//   // });
   
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
+//   $.each( data, function( key, val ) {
+//     items.push( "<li id='" + key[0] + "'>" + val[0] + "</li>" );
+//   });
  
-  $( "<ul/>", {
-    //"class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
+//   $( "<ul/>", {
+//     //"class": "my-new-list",
+//     html: items.join( "" )
+//   }).appendTo( "body" );
+// });
+
+
+
+
+// (function() {
+//   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+
+//   $.getJSON( flickerAPI, {
+//     tags: "arsenal",
+//     format: "json"
+//   })
+//     .done(function( data ) {
+//       $.each( data.items, function( i, item ) {
+//         $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+//         if ( i === 3 ) {
+//           return false;
+//         }
+//       });
+//     });
+// })();
