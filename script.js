8d2fc6b6121744db4c32cb3c0d7231739e22713b
sam11385 <samluedke@gmail.com>
2016-11-23 09:16:03 -0500

    Working on getting some json

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Let us get the json
+
+$.getJSON( "epl.json", function( data ) {
+  var items = [];
+  
+  JSON.stringify(data);
+  
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  });
+ 
+  $( "<ul/>", {
+    //"class": "my-new-list",
+    html: items.join( "" )
+  }).appendTo( "body" );
+});
