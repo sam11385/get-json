81c448a353e4d493ec5226c3b1d68fd412e4a1eb
sam11385 <samluedke@gmail.com>
2017-01-30 07:54:39 -0500

    set temp conversion to fahrenheit

--- get-field-vals.js
+++ get-field-vals.js
@@ -14,20 +14,25 @@ $(document).ready(function() {
 
       $.getJSON(weatherUrl, function(data){
         $('.temp-city').html(data.name);
-        $('.temp-country').html(data.weather);
+
+        for ( var i in data.weather) {
+      		var id = data.weather[i].main;
+      		var name = data.weather[i].description;
+      		$('.temp-weather').html(id);
+      	}
+
+        for ( var i in data.main) {
+      		var temperature = data.main.temp;
+          $('.temp-temp').html((temperature - 273)* 9 / 5 + 32);
+      	}
+
+
       });
 
       $('#text_reset').click(function() {
     	  $('#text').val('');
         weatherUrl.val('');
-        //$('.display').html('');
       });
   	}
   });
-
-  var url ="http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6ba9fe7b01ace29efd87f6336dcd299b"
-  $.getJSON(url, function(data){
-    console.log(data.weather[1]);
-  });
-
 });
