277379333ce35591fb792a44c5b68d47b842d76e
sam11385 <samluedke@gmail.com>
2017-03-05 11:21:00 -0500

    Use gulp clean css node module to minify css, point strava file to dist minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
 var sass = require('gulp-sass');
 gulp.task('sass', function(){
   return gulp.src('scss/styles.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('css/'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
 });
 
 
