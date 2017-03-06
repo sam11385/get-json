f6abd66e5b001cfd00267de88c3528ea758dd7a3
sam11385 <samluedke@gmail.com>
2017-03-06 08:00:14 -0500

    update gulpfile to watch for scss changes

--- gulpfile.js
+++ gulpfile.js
@@ -14,5 +14,22 @@ gulp.task('minify-css', function() {
     .pipe(gulp.dest('dist'));
 });
 
+// Watch
+gulp.task('watch', function() {
+
+    // Watch .scss files
+    gulp.watch('scss/**/*.scss', function(event) {
+      gulp.run('sass');
+      gulp.run('minify-css');
+    });
+
+    // // Watch .js files
+    // gulp.watch('src/scripts/**/*.js', function(event) {
+    //   console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    //   gulp.run('scripts');
+    // });
+
+});
+
 
 gulp.task('default', function() {})
