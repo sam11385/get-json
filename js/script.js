429c7bd7bbad36dd95e0b6f8abd51b26af441d82
sam11385 <samluedke@gmail.com>
2017-03-12 17:13:28 -0400

    clean up some JS

--- js/script.js
+++ js/script.js
@@ -200,7 +200,6 @@ $(document).ready(function(){
 
   // TV Data
   // http://www.tvmaze.com/api
-  // Example of getting some shows: http://api.tvmaze.com/search/shows?q=last-man
   // Example of getting a schedule for a given date: http://api.tvmaze.com/schedule?country=US&date=2014-12-01
   // Example of searching for a tv actor: http://api.tvmaze.com/search/people?q=tim-allen
   if ($('body').hasClass('tv')) {
