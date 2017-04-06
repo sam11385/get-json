6c0b1eb29849f7c18c363890cece25cd4093b753
sam11385 <samluedke@gmail.com>
2017-04-06 13:43:24 -0400

    start creating scss, point cli to using scss and global scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
