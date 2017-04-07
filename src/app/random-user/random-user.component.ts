92d28b0c4da9e2bc8c59d67c40f80485d996ebc0
sam11385 <samluedke@gmail.com>
2017-04-07 11:43:45 -0400

    start trying out using observables to get some data. it is working but need to figure out how to use it for multidimensional arrays and objects and all that goodness.

--- src/app/random-user/random-user.component.ts
+++ src/app/random-user/random-user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'app-random-user',
@@ -6,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./random-user.component.css']
 })
 export class RandomUserComponent implements OnInit {
-
-  constructor() { }
+  private users = [];
+  constructor(http: Http) {
+    http.get('http://jsonplaceholder.typicode.com/users/')
+    .flatMap((data) => data.json())
+      .subscribe((data) => {
+        this.users.push(data);
+      });
+  }
 
   ngOnInit() {
   }
