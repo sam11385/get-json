add44ccea3902a1ba5e6f542339ea9132ad4dc93
sam11385 <samluedke@gmail.com>
2017-04-21 19:43:47 -0400

    import jsonp and observable to user service

--- src/app/random-user/user-service.service.ts
+++ src/app/random-user/user-service.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Jsonp } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class UserServiceService {
