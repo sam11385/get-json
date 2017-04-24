e4ff9e2c9ee0463baeeaf9a63b3ef9fc72c74a0e
sam11385 <samluedke@gmail.com>
2017-04-24 16:44:32 -0400

    remove unnecessary animations code in prep for router animations module

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,29 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { trigger, state, style, animate, group, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css'],
-  animations: [
-    trigger('itemAnim', [
-      transition(':enter', [
-        style({transform: 'translateX(-100%)'}),
-        animate(350)
-      ]),
-      transition(':leave', [
-        group([
-          animate('0.2s ease', style({
-            transform: 'translate(150px,25px)'
-          })),
-          animate('0.5s 0.2s ease', style({
-            opacity: 0
-          }))
-        ])
-      ])
-    ])
-  ]
+  styleUrls: ['./navigation.component.css']
 })
 
 export class NavigationComponent implements OnInit {
