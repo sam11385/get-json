fd383426ab645e4dceb6aea4912e803469ca37c8
sam11385 <samluedke@gmail.com>
2017-05-05 14:43:04 -0400

    Add image correctly - it needs to be defined as a type

--- src/app/random-user/random-user.component.ts
+++ src/app/random-user/random-user.component.ts
@@ -9,17 +9,17 @@ import { Observable } from 'rxjs/Observable';
 })
 export class RandomUserComponent implements OnInit {
   private users = [];
+  private images = [];
   constructor(http: Http) {
-    http.get('https://randomuser.me/api/?results=20')
-    .map((data) => data.json())
+    http.get('https://randomuser.me/api/?results=20').map((data) => data.json())
       .subscribe((data) => {
         for (let i = 0; i < data.results.length; i++) {
           const authFirstName = data.results[i].name.first;
           const authLastName = data.results[i].name.last;
           const authFullName = authFirstName +' '+ authLastName;
-          //const authImg = data.results[i].picture.large;
+          const authImg = data.results[i].picture.large;
           this.users.push(authFullName);
-          //this.users.push(authImg);
+          this.images.push(authImg);
         }
       });
   }
