390683a6bffec4d71c407c9c86220e8cda819e31
sam11385 <samluedke@gmail.com>
2017-05-18 10:27:05 -0400

    Add types to weather service and import http, rxjs operators.

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+const appid = '6ba9fe7b01ace29efd87f6336dcd299b';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  // These details are straight from the JSON - picking out which ones we want to display
+  temp: number;
+  weather: number;
+  humidity: number;
+  temp_min: number;
+  temp_max: number;
+
+  constructor(private http: Http) { }
 
 }
