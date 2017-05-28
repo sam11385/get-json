468284993d8d693d75f7d56a818b04fb3ac80edf
sam11385 <samluedke@gmail.com>
2017-05-28 10:31:31 -0400

    Update nav css, restructure weather service code

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -5,8 +5,6 @@ import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import { WeatherModel, WeatherDetails, WeatherInfo, Sys } from '../weather/weather';
 
-const appid = '6ba9fe7b01ace29efd87f6336dcd299b';
-
 @Injectable()
 export class WeatherService {
   result$: Observable<string[]>;
@@ -14,11 +12,10 @@ export class WeatherService {
   constructor(private http: Http) {
   }
   getWeather(zip):Observable<WeatherModel>{
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&appid='+appid+'').map(response => response.json() as WeatherModel);
+    const appid = '6ba9fe7b01ace29efd87f6336dcd299b';
+    const url = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&appid='+appid+'';
+    return this.http.get(url)
+                    .map(response => response.json() as WeatherModel);
   }
-
-  // getWeather(zip: number):Observable<WeatherModel>{
-  //   return this.http.get('http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid='+appid+'').map(response => response.json() as WeatherModel);
-  // }
 
 }
