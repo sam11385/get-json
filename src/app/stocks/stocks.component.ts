1760ff46c19a4c1e71eab3b63cf449fc37a24251
sam11385 <samluedke@gmail.com>
2017-05-30 11:48:42 -0400

    Add stocks service and create stocks class

--- src/app/stocks/stocks.component.ts
+++ src/app/stocks/stocks.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StocksComponent implements OnInit {
 
+  //const symbol = 'dks';
+  //const stockUrl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol='+symbol+'&callback=?';
+  // http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=DKS
+
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/stocks/stocks.ts
@@ -0,0 +1,8 @@
+export class Stocks {
+  name: string;
+  symbol: string;
+  lastPrice: number;
+  high: number;
+  low: number;
+  open: number;
+}
