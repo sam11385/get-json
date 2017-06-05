e29a927404ace1e8f2faacedfbe4e028e5857883
sam11385 <samluedke@gmail.com>
2017-06-04 20:55:50 -0400

    stocks service is not invoking jsonp callback?

--- src/app/services/stocks.service.ts
+++ src/app/services/stocks.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, Jsonp } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { Stocks } from '../stocks/stocks';
@@ -7,10 +7,10 @@ import { Stocks } from '../stocks/stocks';
 @Injectable()
 export class StocksService {
 
-  constructor(private http:Http) { }
+  constructor(private jsonp: Jsonp) { }
   getStocks(symbol):Observable<Stocks>{
-    const stockUrl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol='+symbol+'';
-    return this.http.get(stockUrl)
+    const stockUrl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol='+symbol+'&?callback=JSONP_CALLBACK';
+    return this.jsonp.get(stockUrl)
                     .map(response => response.json() as Stocks);
   }
 
